d0766905d2198533d4a67dee640b95af23db1928
Khushi <khushigarg.ai24@jecrc.ac.in>
2022-03-23 13:36:03 +0530

    Initial commit

---
+++ src/mainpage/Navbar/MenuItems.js
@@ -0,0 +1,26 @@
+export const MenuItems = [{
+        title: 'Home',
+        url: './main/#Home',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Services',
+        url: './main/#Services',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About',
+        url: './main/#About',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact',
+        url: './main/#Contact',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Log/Register',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+]
